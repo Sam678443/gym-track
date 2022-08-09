b0197f01297f01bd3bf28f6390123f1564ce7072
Sam McClenaghan <sam@aream.ca>
2022-08-09 13:17:38 -0600

    Added user_id to workout table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_162712) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_191111) do
   create_table "exercise_types", force: :cascade do |t|
     t.string "type"
     t.datetime "created_at", null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_162712) do
     t.date "starts_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_workouts_on_user_id"
   end
 
   add_foreign_key "exercises", "exercise_types"
